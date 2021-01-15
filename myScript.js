d0e2b0eb5970ffbaf1a19b3d66f19270cc9514bb
wsinyee2000 <62838083+wsinyee2000@users.noreply.github.com>
2021-01-16 00:08:01 +1100

    complete the task

--- myScript.js
+++ myScript.js
@@ -1,14 +1,46 @@
-var timeKeeper = document.getElementById("timer");
-var starter = document.getElementById("start-btn");
+let second = document.getElementById("second");
+let millisecond = document.getElementById("millisecond");
+let timer = {
+    sec: 00,
+    millisecond: 00
+};
+var stopThis;
 
-//begin
+//Printing out the time from start function
+function printTimer() {
+    if(timer.millisecond < 99){
+        timer.millisecond += 1;
+    } else {
+        timer.millisecond = 00;
+        timer.sec += 1;
+    }
+    second.innerHTML = ("0" + timer.sec).slice(-2);
+    millisecond.innerHTML = ("0" + timer.millisecond).slice(-2);
 
-function startFunction() {
-    console.log('start');
 }
 
-starter.addEventListener("click", startFunction);
+//Start counting
+function startFunction() {
+    
+       stopThis = setInterval(printTimer, 10);
+    
+}
+
+//reset the timer and stop the timer
+function resetFunction() {
+    clearInterval(stopThis);
+    timer.sec = 00;
+    timer.millisecond = 00;
+    second.innerHTML = ("0" + timer.sec).slice(-2);
+    millisecond.innerHTML = ("0" + timer.millisecond).slice(-2);
+
+}
 
 
-// document.getElementById("stop-btn").addEventListener("click", stopFunction);
-// document.getElementById("reset-btn").addEventListener("click", resetFunction);
+//buttons
+document.getElementById("start-btn").addEventListener("click", startFunction);
+document.getElementById("stop-btn").addEventListener("click", () => {
+    clearInterval(stopThis);
+} 
+);
+document.getElementById("reset-btn").addEventListener("click", resetFunction);
