958e0970f660ef58a4ccacb41d4a97662b405b39
wsinyee2000 <62838083+wsinyee2000@users.noreply.github.com>
2021-01-15 21:17:09 +1100

    try to commit

--- myScript.js
+++ myScript.js
@@ -6,9 +6,9 @@ var starter = document.getElementById("start-btn");
 function startFunction() {
     console.log('start');
 }
-// document.onload = () => {
+
 starter.addEventListener("click", startFunction);
-// }
+
 
 // document.getElementById("stop-btn").addEventListener("click", stopFunction);
 // document.getElementById("reset-btn").addEventListener("click", resetFunction);
