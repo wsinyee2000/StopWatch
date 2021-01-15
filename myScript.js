6f5cd84947ab5d04ec0af15043e7760498de508b
wsinyee2000 <62838083+wsinyee2000@users.noreply.github.com>
2021-01-15 21:11:24 +1100

    first commit

---
+++ myScript.js
@@ -0,0 +1,14 @@
+var timeKeeper = document.getElementById("timer");
+var starter = document.getElementById("start-btn");
+
+
+
+function startFunction() {
+    console.log('start');
+}
+// document.onload = () => {
+starter.addEventListener("click", startFunction);
+// }
+
+// document.getElementById("stop-btn").addEventListener("click", stopFunction);
+// document.getElementById("reset-btn").addEventListener("click", resetFunction);
