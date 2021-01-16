bef9b6e213ea7cdef3c1fd04e86e9148b9ff610d
wsinyee2000 <62838083+wsinyee2000@users.noreply.github.com>
2021-01-17 00:26:24 +1100

    Change Record from <p> to table (lapTime & split)

--- myScript.js
+++ myScript.js
@@ -1,8 +1,10 @@
-let second = document.getElementById("second");
-let millisecond = document.getElementById("millisecond");
+let showTime = document.getElementById("timer");
 let timer = {
+    min: 00,
     sec: 00,
-    millisecond: 00
+    millisecond: 00,
+    lap: 0,
+    lastSave: "00:00:00"
 };
 var stopThis;
 
@@ -10,12 +12,14 @@ var stopThis;
 function printTimer() {
     if(timer.millisecond < 99){
         timer.millisecond += 1;
-    } else {
+    } else if (timer.sec < 60){
         timer.millisecond = 00;
         timer.sec += 1;
+    } else {
+        timer.sec = 00;
+        timer.min += 1;
     }
-    second.innerHTML = ("0" + timer.sec).slice(-2);
-    millisecond.innerHTML = ("0" + timer.millisecond).slice(-2);
+    showTime.innerHTML =("0" + timer.min).slice(-2)+":"+ ("0" + timer.sec).slice(-2) + ":" + ("0" + timer.millisecond).slice(-2);
 
 }
 
@@ -32,19 +36,59 @@ function startFunction() {
 //reset the timer and stop the timer
 function resetFunction() {
     clearInterval(stopThis);
+    timer.lap= 0;
+    timer.lastSave= "00:00:00";
     timer.sec = 00;
     timer.millisecond = 00;
-    second.innerHTML = ("0" + timer.sec).slice(-2);
-    millisecond.innerHTML = ("0" + timer.millisecond).slice(-2);
-    document.getElementById("timeRecord").innerHTML = "";
+    showTime.innerHTML = "00:00:00";
+    document.getElementById("timeRecord_body").innerHTML = "";
+    document.getElementById("reset-btn").style.display = "none";
+    document.getElementById("timeRecord").style.display = "none";
+}
+
+//find out the time different
+function timeDifferent(start, end) {
+    let endTime = end.split(":");
+    let startTime = start.split(":"); 
+
+    let endTimeMS = parseInt((endTime[0]*60*1000)) + parseInt((endTime[1] * 1000)) + parseInt(endTime[2]);
+  let startTimeMS = parseInt((startTime[0]*60*1000)) + parseInt((startTime[1] * 1000)) + parseInt(startTime[2]);
+    
+    let timeDiff = endTimeMS - startTimeMS;
+    
+    let timeDiffMin = Math.trunc(timeDiff /60000) ;
+    let timeDiffSec = Math.trunc(((timeDiff %60000)) / 1000);
+    let timeDiffMs = Math.trunc(((timeDiff %60000)) % 1000);
+
+    return ("0" + timeDiffMin).slice(-2) + ":" + ("0" + timeDiffSec).slice(-2) + ":" + ("0" + timeDiffMs).slice(-2);
+
 }
 
 //save records
 function saveRecord() {
+    document.getElementById("timeRecord").style.display = "table";
+    timer.lap += 1;
     
-    var newRecord = document.createElement("p");
-    newRecord.innerHTML = ("0" + timer.sec).slice(-2) + ":" + ("0" + timer.millisecond).slice(-2);
-    document.getElementById("timeRecord").appendChild(newRecord);
+    var currentTime = ("0" + timer.min).slice(-2)+":"+("0" + timer.sec).slice(-2) + ":" + ("0" + timer.millisecond).slice(-2);
+    
+    var newRecord = document.createElement("tr");
+    document.getElementById("timeRecord_body").appendChild(newRecord);
+    
+    var newLap = document.createElement("td");
+    newLap.innerHTML = ("0" + timer.lap).slice(-2);
+    
+    var newLapTime = document.createElement("td");
+    newLapTime.innerHTML = timeDifferent(timer.lastSave, currentTime);
+    
+    var newSplitTime = document.createElement("td");
+    newSplitTime.innerHTML = ("0" + timer.min).slice(-2)+":"+("0" + timer.sec).slice(-2) + ":" + ("0" + timer.millisecond).slice(-2);
+    
+    newRecord.appendChild(newLap);
+    newRecord.appendChild(newLapTime);
+    newRecord.appendChild(newSplitTime);
+
+    timer.lastSave = ("0" + timer.min).slice(-2)+":"+("0" + timer.sec).slice(-2) + ":" + ("0" + timer.millisecond).slice(-2);
+
 }
 
 //pause the timer
@@ -60,7 +104,6 @@ function pauseTimer() {
 //buttons
 document.getElementById("start-btn").addEventListener("click", startFunction);
 document.getElementById("lap-btn").addEventListener("click", () => {
-    // clearInterval(stopThis);
     saveRecord();
 });
 document.getElementById("reset-btn").addEventListener("click", resetFunction);
