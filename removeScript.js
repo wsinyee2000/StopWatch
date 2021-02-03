831170e735501dd67a312117fe3a705e8ef05307
wsinyee2000 <62838083+wsinyee2000@users.noreply.github.com>
2021-02-03 21:23:10 +1100

    Update the lap number after remove row

--- removeScript.js
+++ removeScript.js
@@ -1,9 +1,22 @@
 const removeRecord = () => {
     let td = event.target.parentNode; 
     let tr = td.parentNode; 
+    let tbody = tr.parentNode;
+    //find last row number
+    let lastRowNumber = tbody.firstChild.firstChild.innerHTML;
+   
     //find row number
-    let rowsnumber = tr.firstChild.innerHTML;
-    console.log(rowsnumber);
+    let rowsnumber = parseInt(tr.firstChild.innerHTML);
+    // find how many row need to update
+    let ChangeLapNumber = parseInt(lastRowNumber-rowsnumber);
+
+
+    //update lap numbers
+    for(let i=ChangeLapNumber; i>=0; i--){
+        tbody.childNodes[i].firstChild.innerHTML = setTwoDigit(rowsnumber-1);
+        rowsnumber += 1;
+    }
+
     //remove row
     tr.parentNode.removeChild(tr);
 
